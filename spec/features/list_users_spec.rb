9db5ddb463026a19d8127ef9502810a47a5ebc4c
De Wet Blomerus <dewet@blomerus.org>
2014-10-06 20:11:07 +0200

    list users

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
-describe "Viewing the list of users" do
-  
-  it "shows the users" do
-    visit 'http://example.com/users'
+def user_attributes(overrides = {})
+  {
+    name: "Example User",
+    email: "user@example.com",
+    password: "secret",
+    password_confirmation: "secret"
+  }.merge(overrides)
+end
 
-    expect(page).to have_text("3 Users")
+describe "Viewing the list of users" do
+
+  it "shows the users" do
+    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com"))
+    user2 = User.create!(user_attributes(name: "Moe",   email: "moe@example.com"))
+    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com"))
+    
+    visit users_url
+    
+    expect(page).to have_link(user1.name)
+    expect(page).to have_link(user2.name)
+    expect(page).to have_link(user3.name)
   end
-  
+
 end
 
 
