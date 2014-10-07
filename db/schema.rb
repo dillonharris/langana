4020987d9f659a0397d513e6727255ed7e3bba73
De Wet Blomerus <dewet@blomerus.org>
2014-10-07 20:00:54 +0200

    fixed duplicate migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006180654) do
+ActiveRecord::Schema.define(version: 20141004201157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20141006180654) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
