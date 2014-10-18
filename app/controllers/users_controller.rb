e24b820698c8c8cc4e9e1dd3a1831527b695af76
De Wet Blomerus <dewet@blomerus.org>
2014-10-18 11:33:30 +0200

    beginnings of authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :require_signin, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -5,7 +5,7 @@ describe "Viewing an individual user" do
   it "shows the user's details" do    
     user = User.create(user_attributes)
 
-    visit "http://example.com/users/#{user.id}"
+    visit user_url(user)
 
     expect(page).to have_text(user.name)
     expect(page).to have_text(user.email)
