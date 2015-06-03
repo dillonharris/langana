252ce86728228ae8376d1da367708f2ac20a8479
De Wet Blomerus <dewet@blomerus.org>
2015-06-03 10:34:48 +0200

    show only first name in header

--- spec/features/create_users_spec.rb
+++ spec/features/create_users_spec.rb
@@ -18,7 +18,7 @@ describe "Creating a new user" do
 
     expect(current_path).to eq(user_path(User.last))
 
-    expect(page).to have_text('Usie Userson')
+    expect(page).to have_text('Usie')
     expect(page).to have_text('Thanks for signing up!')
     expect(page).not_to have_link('Sign In')
     expect(page).not_to have_link('Sign Up')
