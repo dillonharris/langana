6c4f74fed7a5aef1dd4ab1c0681bc3240e39fbe1
De Wet Blomerus <dewet@blomerus.org>
2015-08-23 11:36:09 +0200

    Show confirm link to unconfirmed users

--- spec/features/confirm_user_spec.rb
+++ spec/features/confirm_user_spec.rb
@@ -5,14 +5,10 @@ describe "Confirming a mobile number" do
   xit "confirms with the correct mobile confirmation token" do
     user = FactoryGirl.create(:user, mobile_confirmation_token: "abcde")
     expect(user.confirmed_at).to be_nil
-
     sign_in(user)
     visit confirm_user_path(user)
-
     fill_in "Mobile confirmation token", with: "abcde"
-
     click_button "Confirm Mobile Number"
-
     expect(page).to have_text("Thanks for confirming your mobile number!")
     # The code is working as expected but this next line fails, I need help
     expect(user.confirmed_at).not_to be_nil
@@ -21,14 +17,10 @@ describe "Confirming a mobile number" do
   it "does not confirm with the incorrect mobile confirmation token" do
     user = FactoryGirl.create(:user, mobile_confirmation_token: "abcde", confirmed_at: nil)
     expect(user.confirmed_at).to be_nil
-
     sign_in(user)
     visit confirm_user_path(user)
-
     fill_in "Mobile confirmation token", with: ""
-
     click_button "Confirm Mobile Number"
-
     expect(page).to have_text("Incorrect confirmation token")
     expect(user.confirmed_at).to be_nil
   end
@@ -36,10 +28,15 @@ describe "Confirming a mobile number" do
   xit "resends verification code when the user requests it" do
     user = FactoryGirl.create(:user, mobile_confirmation_token: "abcde")
     expect(user.confirmed_at).to be_nil
-
     sign_in(user)
     visit confirm_user_path(user)
     # This next line fails even though the notice appears in browser, I need help
     expect(page).to have_text("We sent it again")
+  end
+
+  it "show a button to confrim in the header if user is not confirmed" do
+    user = FactoryGirl.create(:user, mobile_confirmation_token: "abcde", confirmed_at: nil)
+    sign_in(user)
+    expect(page).to have_text("Confirm phone number")
   end
 end
