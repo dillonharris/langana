7d9b7cb750f0e0f3a391c1cac4f2a3943ee703e4
De Wet Blomerus <dewet@blomerus.org>
2015-09-20 15:11:37 +0200

    test listing confirmed workers only

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe "Viewing the list of users" do
 
-  xit "shows only the worker users with confirmed mobile numbers" do
+  it "shows only the worker users with confirmed mobile numbers" do
     legit_worker1 = FactoryGirl.create(:user, role: :worker, first_name: "LegitOne", mobile_number: "0791237894")
     legit_worker2 = FactoryGirl.create(:user, role: :worker, first_name: "LegitTwo", mobile_number: "0791237895")
     legit_worker3 = FactoryGirl.create(:user, role: :worker, first_name: "LegitThree", mobile_number: "0791237896")
     employer = FactoryGirl.create(:user, role: :employer, first_name: "Emplo",   mobile_number: "0799999999")
-    unconfirmed_worker = FactoryGirl.create(:user, role: :worker, first_name: "Unconfirmedworker", mobile_number: "0791239632")
+    unconfirmed_worker = FactoryGirl.create(:user, role: :worker, first_name: "Unconfirmedworker", mobile_number: "0791239632", confirmed_at: nil)
 
     visit users_url
     expect(page).to have_link(legit_worker1.first_name)
