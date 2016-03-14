2da4716ee4bbfdcc22d0b0bf3c1da2b8dd6945e3
De Wet <dewet@blomerus.org>
2016-03-14 15:25:21 +0200

    send sms ssl = false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # added this line to try and chase a bug with sending sms
+    config.force_ssl = false
 end
