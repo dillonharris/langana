1d1a6f74ecc3f94691207d9456bef305a83758eb
De Wet <dewet@blomerus.org>
2016-04-15 22:14:23 +0200

    shorteen a few lines

--- spec/features/list_worker_reviews_spec.rb
+++ spec/features/list_worker_reviews_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
-describe 'Given a worker called Kenwin with a reference from another user called De Wet' do
+describe 'Given worker called Kenwin with reference from user called De Wet' do
   before do
     @dewet = FactoryGirl.create(:user, first_name: 'De Wet', mobile_number: '0792951234')
-    @kenwin = FactoryGirl.create(:worker, first_name: 'Kenwin',   mobile_number: '0729251234')
-    @review = WorkReference.create work: 'IT', comment: 'The best IT work ever!!!', worker: @kenwin, employer_user: @dewet
+    @kenwin = FactoryGirl.create(:worker, first_name: 'Kenwin', mobile_number: '0729251234')
+    @review = WorkReference.create(
+      work: 'IT',
+      comment: 'The best IT work ever!!!',
+      worker: @kenwin,
+      employer_user: @dewet
+    )
   end
 
-  describe 'when users gives reviews to a worker it lists the reviews on their profile page' do
+  describe 'when users gives reviews, it lists the reviews on profile page' do
     it 'lists reviews' do
       sign_in(@dewet)
 
